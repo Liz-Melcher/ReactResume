fadd96b6dfcf1814e16279a06eefdb9a6ee7f7a4
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-03-17 21:47:09 -0500

    updated avatar in home.jsx

--- ReactResume/src/pages/Home.jsx
+++ ReactResume/src/pages/Home.jsx
@@ -1,7 +1,20 @@
+// WHEN I load the portfolio the first time
+// THEN the About Me title and section are selected by default
+// WHEN I am presented with the About Me section
+// THEN I see a recent photo or avatar of the developer and a short bio about them
+
+import avatar from "../assets/images/IMG_4134.jpeg"
+
 export default function Home() {
     return (
         <div className="container mt-5">
             <div className="text-center p-4 bg-light rounded shadow-sm">
+                <img 
+                    src={avatar} 
+                    alt="Developer Avatar" 
+                    className="img-thumbnail rounded-circle d-block mx-auto mb-3"
+                    style={{ width: "150px" }} // Controls image size
+                />
                 <h2 className="mb-3">About Me</h2>
                 <p className="lead fs-5">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi sunt rerum soluta et perspiciatis velit aliquam reprehenderit atque, doloremque eligendi assumenda magnam doloribus quaerat molestias hic corrupti facilis earum quis.
