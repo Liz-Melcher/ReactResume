95078155a0d4c6f3f4829d89b64121dd56cd237e
Liz Melcher <lizmelchercodingbootcamp@gmail.com>
2025-03-18 18:21:48 -0500

    fixed nav tabs

--- ReactResume/src/components/NavTabs.jsx
+++ ReactResume/src/components/NavTabs.jsx
@@ -5,54 +5,38 @@
 // WHEN I click on a navigation title
 // THEN the browser URL changes and I am presented with the corresponding section below the navigation and that title is highlighted
 
+import { Navbar, Nav, Container } from "react-bootstrap"; // use React Bootstrap so the menu works as a drop down on mobile
 import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"; // Ensure Bootstrap is imported
 
 function NavTabs() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div className="container">
-        <NavLink className="navbar-brand" to="/">
+    <Navbar expand="lg" bg="primary" variant="dark" className="shadow">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">
           Liz M Portfolio
-        </NavLink>
+        </Navbar.Brand>
 
-        <button 
-          className="navbar-toggler" 
-          type="button" 
-          data-bs-toggle="collapse" 
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav" 
-          aria-expanded="false" 
-          aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <Navbar.Toggle aria-controls="navbarNav" />
 
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <NavLink to="/" className="nav-link" activeclassname="active">
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/resume" className="nav-link" activeclassname="active">
-                Resume
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/portfolio" className="nav-link" activeclassname="active">
-                Portfolio
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/contact" className="nav-link" activeclassname="active">
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+        <Navbar.Collapse id="navbarNav">
+          <Nav className="ms-auto">
+            <Nav.Link as={NavLink} to="/" activeclassname="active">
+              Home
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/resume" activeclassname="active">
+              Resume
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/portfolio" activeclassname="active">
+              Portfolio
+            </Nav.Link>
+            <Nav.Link as={NavLink} to="/contact" activeclassname="active">
+              Contact
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
